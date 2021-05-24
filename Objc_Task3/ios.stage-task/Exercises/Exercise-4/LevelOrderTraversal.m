24824399b3d6a56dd584e4579e399790fc3252aa
brazilec22 <stratienko.artem@gmail.com>
2021-05-24 23:54:13 +0300

    ObjC Task 4 - Part 1,2,3

--- Objc_Task3/ios.stage-task/Exercises/Exercise-4/LevelOrderTraversal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-4/LevelOrderTraversal.m
@@ -1,17 +1,26 @@
 #import "LevelOrderTraversal.h"
 
 NSArray *LevelOrderTraversalForTree(NSArray *tree) {
-    NSMutableArray *tmp;
-    tmp = [[ NSMutableArray alloc ] init ];
+    NSMutableArray *leftNode;
+    leftNode = [ NSMutableArray new ];
+    NSMutableArray *rightNode;
+    rightNode = [ NSMutableArray new ];
+    NSInteger LevelNode = 0;
     NSMutableArray *res;
-    res = [[ NSMutableArray alloc ] init ];
+    res = [ NSMutableArray new ];
+   
     for (  NSNumber *itr in tree ) {
         if ( [ itr isKindOfClass: NSArray.class ]) {
             
         } else if ( ![ itr isEqual : [NSNull null] ] ) {
-            [ tmp removeAllObjects ];
-            [ tmp addObject: itr ];
-            [ res addObject: tmp];
+            [ res addObject: [ NSArray arrayWithObject: itr ] ];
+            if ( LevelNode == 2 ) {
+                //      [ leftNode addObject: [ NSArray arrayWithObject: itr ] ];
+                //      [ rightNode addObject: [ NSArray arrayWithObject: itr ] ];
+                //      [ res addObject: [ NSArray arrayWithObject: itr ] ];
+            }
+        } else {
+            LevelNode += 1;
         }
     }
     return res;
